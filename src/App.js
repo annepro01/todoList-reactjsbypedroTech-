bde676a35b749aa9e8212955ec0a01246df6f76c
annepro01 <mary.stoneworks@gmail.com>
2022-03-23 16:49:18 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
-import React, { useState } from "react"
-import "./app.css"
+import React, { useState } from "react";
+import "./app.css";
 
-function App() {
 
-  const [toggle, setToggle] = useState(false);
 
-  const toggleFunc = () => {
-    setToggle(!toggle)
+
+const App = () => {
+
+  const [todoList, setTodoList] = useState([]); // todolist where we store our data, setTodoList is where we set our todoList
+  const [currentTask, setCurrentTask] = useState(""); // currentTask the actual task that we write, setCurrentTask is where we set the actual task and store
+
+    
+  const addTask = () => {
+    setTodoList([...todoList, currentTask]);
+    console.log(todoList)
+  };
+
+
+  const handleOnChange = (event) => {
+      setCurrentTask(event.target.value)
   }
-  console.log("update!")
-  return (
-    <div className={toggle ? "container change" : "container"}>
-      <button onClick={toggleFunc}>TOGGLE</button>
 
-    </div>
-  ) 
+  return (
+      <div className="App">
+        <h1>Todo List</h1>
+          <div>
+            <input type="text" placeholder="Task" onChange={handleOnChange}/> 
+            <button onClick={addTask}>ADD TASK</button>
+          </div>
+          <hr/>
+          <ul>
+              {todoList.map((value, key) => {
+                  return (
+                    <li>
+                      {value}
+                    </li>
+                  )
+              })}
+
+          </ul>
+
+      </div>
+  )
 }
 
 export default App;
